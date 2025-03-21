25b3d7c1240424037efde75e3f5042fef8e87226
sendy <sendy@duakelinci.com>
2025-03-21 20:57:25 +0700

    Add MembershipSeeder to populate membership plans in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\UserAndPermissionSeeder;
+use Database\Seeders\MembershipSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             UserAndPermissionSeeder::class,
+            MembershipSeeder::class,
         ]);
     }
 }
