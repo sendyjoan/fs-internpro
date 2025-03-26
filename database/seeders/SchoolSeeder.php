9e37c699b8ec51c337ef3cd58fcd3242403391a6
sendy <sendy@duakelinci.com>
2025-03-26 13:04:25 +0700

    Add membership handling in SchoolSeeder; create SchoolMembershipSummary for new school

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Membership;
 use App\Models\User;
 use App\Models\School;
+use App\Models\SchoolMembershipSummary;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -14,13 +16,24 @@ class SchoolSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = User::where('name', 'Administrator')->get();
+        $admin = User::where('name', 'Administrator')->first();
+        $membership = Membership::where('name', 'Free')->first();
         $smk1 = School::create([
             'name' => 'SMKN 1 Trenggalek',
             'email' => 'admin@smk1trenggalek.ac.id',
             'phone' => '081222333444',
             'address' => 'Trenggalek',
             'contact' => 'Bu Mariska',
+            // 'membership_id' => $membership->id,
+            'created_by' => $admin->id,
+            'updated_by' => $admin->id,
+        ]);
+
+        $summary = SchoolMembershipSummary::create([
+            'school_id' => $smk1->id,
+            'membership_id' => $membership->id,
+            'start_membership' => now()->format('Y-m-d'),
+            'end_membership' => now()->addMonth(1)->format('Y-m-d'),
         ]);
     }
 }
