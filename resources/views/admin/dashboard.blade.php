c35365c2457ab54ae0238c6ea56cc260a844592e
sendy <sendy@duakelinci.com>
2025-03-28 11:25:06 +0700

    feat: Integrate class data into dashboard; update view to display total classes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,10 +36,10 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>News</h4>
+                                <h4>Total Class</h4>
                             </div>
                             <div class="card-body">
-                                42
+                                {{ $classes->count() }} / {{ $max_class->membership->max_classes }}
                             </div>
                         </div>
                     </div>
